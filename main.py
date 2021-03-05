2363c3612c4dfad5bea592c47724fec83530c418
Aban Hussain <hsnabn@gmail.com>
2021-03-06 02:39:49 +0500

    make some small aesthetic changes to main script

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import telebot
 
-# Bot API
 key = open('.key').read()
 bot = telebot.TeleBot(key, parse_mode='html')
 
-# Main Bot Commands
 @bot.message_handler(commands=['start'])
-def tictactoe_handler(message):
+def start_handler(message):
     bot.send_message(message.chat.id, "Hello there")
 
 bot.polling() 
